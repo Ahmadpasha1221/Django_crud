2ddaf37fa66fe47c1cb8527b4174787380fc6f4c
Ahmadpasha1221 <ahmadpashashaiks@gmail.com>
2024-12-02 22:15:21 +0530

    Prepare app for deployment

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
+DATABASES['default'] = dj_database_url.config(default='sqlite:///db.sqlite3')
 
 # Application definition
 
